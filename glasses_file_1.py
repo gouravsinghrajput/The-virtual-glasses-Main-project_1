fd11ca32ff5de7c0a200de7812ea76b6d2cc55c0
Gourav Singh Rajput <gsrajput24sept@gmail.com>
2026-04-09 10:51:33 +0530

    starting it

--- glasses_file_1.py
+++ glasses_file_1.py
@@ -1,1 +1,26 @@
+# libraries --------------------------------
 
+import cv2 as cv 
+import mediapipe as mp 
+import numpy as np 
+import pygame as py 
+import pyttsx3 as tts  
+import speech_recognition as sr 
+from gtts import gTTs 
+import time 
+import random
+import datetime
+import threading 
+
+#------------------------------------------
+
+#intializing the window -------------------
+
+cap = cv.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    frame.resize(700, 500)
+    cv.imshow("glasses", frame) 
+
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
